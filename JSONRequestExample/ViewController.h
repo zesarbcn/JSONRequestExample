f37a70ad945d7bfdfdfbb351a9e6a26a26bc402e
Cesar Perez <kaesarbcn@hotmail.com>
2014-03-02 13:58:24 +0100

    Initial Commit

---
+++ JSONRequestExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JSONRequestExample
+//
+//  Created by Cesar Perez Laguna on 02/03/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JSONRequestExampleTests/JSONRequestExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  JSONRequestExampleTests.m
+//  JSONRequestExampleTests
+//
+//  Created by Cesar Perez Laguna on 02/03/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSONRequestExampleTests : XCTestCase
+
+@end
+
+@implementation JSONRequestExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
